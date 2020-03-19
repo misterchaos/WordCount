ab28a50ab9e6cd55ced834180346c36042fa582b
misterchaos <kobe524348@gmail.com>
2020-03-19 21:22:27 +0800

    chore(view):将fxml文件移出view文件夹以便可以打包成exe运行

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,18 +12,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("./view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Word Count | Copyright@2020 黄钰朝");
         primaryStage.setScene(new Scene(root, 1460, 900));
         primaryStage.setResizable(false);
         primaryStage.show();
-
-
     }
 
 
     public static void main(String[] args) {
-        if (Constant.OPS_X.equalsIgnoreCase(args[0])) {
+        if (null == args ||args.length==0|| Constant.OPS_X.equalsIgnoreCase(args[0])) {
             //启动图形界面
             launch(args);
         } else {
