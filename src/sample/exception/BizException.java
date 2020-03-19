6c241702006cfb27773946f2e7591090f06f006e
misterchaos <kobe524348@gmail.com>
2020-03-19 12:28:15 +0800

    feat(exception):添加业务异常类

---
+++ src/sample/exception/BizException.java
@@ -0,0 +1,9 @@
+package sample.exception;
+
+/**
+ * @author <a href="mailto:kobe524348@gmail.com">黄钰朝</a>
+ * @description 业务处理异常，提示给用户
+ * @date 2020-03-19 11:40
+ */
+public class BizException extends RuntimeException{
+}
